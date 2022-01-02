08a680dae0be61df84f93ccaa250ce8ae74bd2b2
Subhadeep Dey <subhadeep1999.de@gmail.com>
2022-01-02 21:39:52 +0530

    day 01 done

---
+++ Day01/script.js
@@ -0,0 +1,21 @@
+//firstname lastname email password confirm password age address
+// variable - let / var/ const
+let firstName = 'Subhadeep';
+let lastName = 'Dey'; // type :- string
+let password = '69';
+let confirmPassword = password;
+let age = 22; // type : number
+let address = 'chandannagar, hooghly, WB';
+
+console.log(age);
+console.log(address);
+console.log(
+	'your first name is',
+	firstName,
+	' and your last name is',
+	lastName
+);
+// string literal
+console.log(
+	`your first name is ${firstName} and your last name is ${lastName}`
+);
