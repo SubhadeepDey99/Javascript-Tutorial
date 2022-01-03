b54f43c50fdc3ce3e20bec82c4604adbb8aae5b7
Subhadeep Dey <subhadeep1999.de@gmail.com>
2022-01-03 21:14:17 +0530

    day 02 added

---
+++ Day02/script.js
@@ -0,0 +1,32 @@
+// let numOne = 10;
+// let numTwo = 50;
+// // let numThree = 80;
+// // let numFour = 120;
+
+// // let sum = numOne + numTwo + numThree + numFour; // 260
+
+// // let division = numFour / numTwo; // 2.4
+
+// // let complex = (numFour - numOne) / (numThree * numTwo); // 0.0275
+
+// // console.log(
+// // 	`The sum is ${sum},division is ${division} and complex is ${complex}`
+// // );
+
+// /*
+// Conditional statemaent types are == , != , > , < , >= , <=
+// */
+
+// // let isEqualTo = numOne == numTwo;
+
+// // console.log(isEqualTo);
+
+let passwords = '2845';
+
+let confirmPassword = '2846';
+
+if (confirmPassword == passwords) {
+	console.log('yayyyyy! youre in');
+} else {
+	console.log('bhaag saala!');
+}
